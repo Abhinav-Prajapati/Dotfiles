878b3d3e21b17dd0b50a29989d20f8da1c2ceb60
Abhinav <abhinavprajapati@gmail.com>
2024-04-30 00:20:53 +0530

    Update polybar launch script to launch multiple instances of polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+pkill -9 polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar bar1 &
+# Launch polybar
+polybar main -c ~/.config/polybar/config.ini &
+polybar main_2 -c ~/.config/polybar/config.ini &
 
-echo "Bars launched..."
